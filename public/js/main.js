07e8791ae18911d3fd70841bf1a6372afe83bcfe
Emrah <emrahketzo@gmail.com>
2024-01-30 19:00:10 +0100

    section 2 et section 3 finies, le html/css/js, désolé j'ai oublié de faire un commit pour la section 2

--- public/js/main.js
+++ public/js/main.js
@@ -11,6 +11,14 @@ let textStarted2 = document.querySelector(".textStarted2");
 let divDrop = document.querySelector(".divDrop");
 let dropListe = document.querySelector(".dropListe");
 
+let readBtn = document.querySelector(".readBtn");
+
+let boxes = document.querySelectorAll(".box");
+let boxesArray = Array.from(boxes);
+console.log(boxesArray);
+let icones = document.querySelectorAll(".fa-solid");
+console.log(boxesArray);
+
 
 // addEventListeners : 
 
@@ -30,10 +38,96 @@ textStarted2.addEventListener("mouseout", () => {
     textStarted2.classList.toggle("transformStarted");
 })
 
+
+// pour le DropDown :
 divDrop.addEventListener("mouseover", () => {
     dropListe.classList.toggle("showDropListe");
 })
 
 divDrop.addEventListener("mouseout", () => {
     dropListe.classList.toggle("showDropListe");
+})
+
+// pour le bouton Read ME
+readBtn.addEventListener("mouseover", () => {
+    readBtn.classList.toggle("readLinear");
+})
+
+readBtn.addEventListener("mouseout", () => {
+    readBtn.classList.toggle("readLinear");
+})
+
+//pour le hover sur les boxes de la section 3
+boxesArray[0].addEventListener("mouseover", () => {
+    boxesArray[0].classList.toggle("colorBlue");
+    boxesArray[0].children[0].children[0].classList.toggle("iconWhite");
+    boxesArray[0].children[1].children[0].classList.toggle("iconWhite");
+    boxesArray[0].children[1].children[1].classList.toggle("iconWhite");
+})
+boxesArray[1].addEventListener("mouseover", () => {
+    boxesArray[1].classList.toggle("colorOrange");
+    boxesArray[1].children[0].children[0].classList.toggle("iconWhite");
+    boxesArray[1].children[1].children[0].classList.toggle("iconWhite");
+    boxesArray[1].children[1].children[1].classList.toggle("iconWhite");
+})
+boxesArray[2].addEventListener("mouseover", () => {
+    boxesArray[2].classList.toggle("colorPink");
+    boxesArray[2].children[0].children[0].classList.toggle("iconWhite");
+    boxesArray[2].children[1].children[0].classList.toggle("iconWhite");
+    boxesArray[2].children[1].children[1].classList.toggle("iconWhite");
+})
+boxesArray[3].addEventListener("mouseover", () => {
+    boxesArray[3].classList.toggle("colorYellow");
+    boxesArray[3].children[0].children[0].classList.toggle("iconWhite");
+    boxesArray[3].children[1].children[0].classList.toggle("iconWhite");
+    boxesArray[3].children[1].children[1].classList.toggle("iconWhite");
+})
+boxesArray[4].addEventListener("mouseover", () => {
+    boxesArray[4].classList.toggle("colorOrangeRed");
+    boxesArray[4].children[0].children[0].classList.toggle("iconWhite");
+    boxesArray[4].children[1].children[0].classList.toggle("iconWhite");
+    boxesArray[4].children[1].children[1].classList.toggle("iconWhite");
+})
+boxesArray[5].addEventListener("mouseover", () => {
+    boxesArray[5].classList.toggle("colorSkyBlue");
+    boxesArray[5].children[0].children[0].classList.toggle("iconWhite");
+    boxesArray[5].children[1].children[0].classList.toggle("iconWhite");
+    boxesArray[5].children[1].children[1].classList.toggle("iconWhite");
+})
+
+boxesArray[0].addEventListener("mouseout", () => {
+    boxesArray[0].classList.toggle("colorBlue");
+    boxesArray[0].children[0].children[0].classList.toggle("iconWhite");
+    boxesArray[0].children[1].children[0].classList.toggle("iconWhite");
+    boxesArray[0].children[1].children[1].classList.toggle("iconWhite");
+})
+boxesArray[1].addEventListener("mouseout", () => {
+    boxesArray[1].classList.toggle("colorOrange");
+    boxesArray[1].children[0].children[0].classList.toggle("iconWhite");
+    boxesArray[1].children[1].children[0].classList.toggle("iconWhite");
+    boxesArray[1].children[1].children[1].classList.toggle("iconWhite");
+})
+boxesArray[2].addEventListener("mouseout", () => {
+    boxesArray[2].classList.toggle("colorPink");
+    boxesArray[2].children[0].children[0].classList.toggle("iconWhite");
+    boxesArray[2].children[1].children[0].classList.toggle("iconWhite");
+    boxesArray[2].children[1].children[1].classList.toggle("iconWhite");
+})
+boxesArray[3].addEventListener("mouseout", () => {
+    boxesArray[3].classList.toggle("colorYellow");
+    boxesArray[3].children[0].children[0].classList.toggle("iconWhite");
+    boxesArray[3].children[1].children[0].classList.toggle("iconWhite");
+    boxesArray[3].children[1].children[1].classList.toggle("iconWhite");
+})
+boxesArray[4].addEventListener("mouseout", () => {
+    boxesArray[4].classList.toggle("colorOrangeRed");
+    boxesArray[4].children[0].children[0].classList.toggle("iconWhite");
+    boxesArray[4].children[1].children[0].classList.toggle("iconWhite");
+    boxesArray[4].children[1].children[1].classList.toggle("iconWhite");
+})
+boxesArray[5].addEventListener("mouseout", () => {
+    boxesArray[5].classList.toggle("colorSkyBlue");
+    boxesArray[5].children[0].children[0].classList.toggle("iconWhite");
+    boxesArray[5].children[1].children[0].classList.toggle("iconWhite");
+    boxesArray[5].children[1].children[1].classList.toggle("iconWhite");
 })
